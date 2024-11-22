045712d91011259f25a04b4f7944c444de537794
sergiohideki <shnakashimanakata@gmail.com>
2024-11-22 11:42:18 -0300

    Aprendendo conversão decimal em Javascript

---
+++ fundamentos/conversão_num.js
@@ -0,0 +1,7 @@
+// este código faz a conversão do numero decimal para binário, octa e hexadecimal respectivamente.
+let num = 15
+
+console.log(num)
+console.log(num.toString(2))
+console.log(num.toString(8))
+console.log(num.toString(16))
